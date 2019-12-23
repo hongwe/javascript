6eddbe24c9351952504ccbcb4632ac755b2bcc23
wanghong <wanghong@teacher.com.cn>
2019-12-23 09:29:09 +0800

    合并笔记

---
+++ myScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementById("dome").innerHTML =
+    "我的第一个 JavaScript 函数";
+}
+var a=2;
+var b=3;
+function myFunction001(a, b) {
+  return a * b;                                // 返回 a 乘以 b 的结果
+}
