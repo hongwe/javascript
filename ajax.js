c6ee7efeb2be35379e5ea336db9e872bbd8a2c0d
wanghong <wanghong@teacher.com.cn>
2019-12-30 17:14:05 +0800

    修改缩进2>4

--- ajax.js
+++ ajax.js
@@ -1,27 +1,25 @@
-function ajax(url,fnSucc,fnFaild ){
+function ajax(url, fnSucc, fnFaild) {
     if (window.XMLHttpRequest) {
         var oAjax = new XMLHttpRequest();
-      } else {
+    } else {
         var oAjax = new ActiveXObject("Microsift.XMLHTTP");
-      }
-    
-      oAjax.open('GET', 'a.txt?t=' + new Data().getTime(),true);
-      oAjax.send();
-      oAjax.onreadystatechange = function() {
-        //oAjax.readyState
-        if ((oAjax.readyState = 4)) //读取完成
-        {
-            if(oAjax.status==200)//成功
-            {
-               fnSucc(oAjax.responseText);
-            }
-            else{
+    }
 
-                if(fnFaild){
-                    fnFaild(oAjax.status)
+    oAjax.open("GET", "a.txt?t=" + new Data().getTime(), true);
+    oAjax.send();
+    oAjax.onreadystatechange = function() {
+        //oAjax.readyState
+        if ((oAjax.readyState = 4)) {
+            //读取完成
+            if (oAjax.status == 200) {
+                //成功
+                fnSucc(oAjax.responseText);
+            } else {
+                if (fnFaild) {
+                    fnFaild(oAjax.status);
                 }
-                alert('失败:' + oAjax.status);
+                alert("失败:" + oAjax.status);
             }
         }
-      };
+    };
 }
--- myScript.js
+++ myScript.js
@@ -1,9 +1,8 @@
 function myFunction() {
-  document.getElementById("dome").innerHTML =
-    "我的第一个 JavaScript 函数";
+    document.getElementById("dome").innerHTML = "我的第一个 JavaScript 函数";
 }
-var a=2;
-var b=3;
+var a = 2;
+var b = 3;
 function myFunction001(a, b) {
-  return a * b;                                // 返回 a 乘以 b 的结果
+    return a * b; // 返回 a 乘以 b 的结果
 }
