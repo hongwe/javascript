6e5aa2b720d47df54155ef0e8b96c28ddf0d95df
wanghong <hongwe@gmail.com>
2020-01-04 09:43:27 +0800

    合并

--- ajax.js
+++ ajax.js
@@ -1,30 +1,25 @@
-function Drag(id) {
-    var _this = this;
-    this.disX = 0;
-    this.disY = 0;
-    this.oDiv = document.getElementById(id);
-    this.oDiv.onmousedown = function(ev) {
-        _this.fnDown(ev);
+function ajax(url, fnSucc, fnFaild) {
+    if (window.XMLHttpRequest) {
+        var oAjax = new XMLHttpRequest();
+    } else {
+        var oAjax = new ActiveXObject("Microsift.XMLHTTP");
+    }
+
+    oAjax.open("GET", "a.txt?t=" + new Data().getTime(), true);
+    oAjax.send();
+    oAjax.onreadystatechange = function() {
+        //oAjax.readyState
+        if ((oAjax.readyState = 4)) {
+            //读取完成
+            if (oAjax.status == 200) {
+                //成功
+                fnSucc(oAjax.responseText);
+            } else {
+                if (fnFaild) {
+                    fnFaild(oAjax.status);
+                }
+                alert("失败:" + oAjax.status);
+            }
+        }
     };
 }
-Drag.prototype.fnDown = function(ev) {
-    var _this = this;
-    var oEvent = ev || event;
-    this.disX = oEvent.clientX - this.oDiv.offsetLeft;
-    this.disY = oEvent.clientY - this.oDiv.offsetTop;
-    document.onmousemove = function(ev) {
-        _this.fnMove(ev);
-    };
-    document.onmouseup = function(ev) {
-        _this.fnUp(ev);
-    };
-};
-Drag.prototype.fnMove = function(ev) {
-    var oEvent = ev || event;
-    this.oDiv.style.left = oEvent.clientX - this.disX + "px";
-    this.oDiv.style.top = oEvent.clientY - this.disY + "px";
-};
-Drag.prototype.fnUp = function() {
-    document.onmousemove = null;
-    document.onmouseup = null;
-};
